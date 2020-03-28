bcd52c930846ecff652748c29b79ded6f6379ca8
topotopo <maxine.micu@gmail.com>
2020-03-28 17:38:38 +0800

    chore: steup fake feed api

--- app/src/main/java/com/topotopo/genericadapterdemo/di/AppModule.kt
+++ app/src/main/java/com/topotopo/genericadapterdemo/di/AppModule.kt
@@ -1,14 +1,20 @@
 package com.topotopo.genericadapterdemo.di
 
+import android.content.Context
+import com.topotopo.genericadapterdemo.BuildConfig
 import com.topotopo.genericadapterdemo.feed.FeedViewModel
 import com.topotopo.genericadapterdemo.repo.FeedRepository
 import com.topotopo.genericadapterdemo.repo.FeedRepositoryImpl
 import com.topotopo.genericadapterdemo.service.FeedApi
+import com.topotopo.genericadapterdemo.service.mock.FakeFeedApi
 import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.mock.BehaviorDelegate
+import retrofit2.mock.MockRetrofit
+import retrofit2.mock.NetworkBehavior
 
 val appModule = module {
     single<FeedRepository> { FeedRepositoryImpl(get()) }
@@ -19,7 +25,7 @@ val appModule = module {
 val networkModule = module {
     factory { provideOkHttpClient() }
     single { provideRetrofit(get()) }
-    factory { provideMoviesApi(get()) }
+    factory { provideMoviesApi(get(), get()) }
 }
 
 fun provideOkHttpClient(): OkHttpClient {
@@ -32,6 +38,15 @@ fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         .addConverterFactory(GsonConverterFactory.create()).build()
 }
 
-fun provideMoviesApi(retrofit: Retrofit): FeedApi {
-    return retrofit.create(FeedApi::class.java)
+fun provideMoviesApi(retrofit: Retrofit, context: Context): FeedApi {
+    return if (BuildConfig.IS_MOCK) {
+        val behavior = NetworkBehavior.create()
+        val mockRetrofit = MockRetrofit.Builder(retrofit).networkBehavior(behavior).build()
+        val delegate: BehaviorDelegate<FeedApi> =
+            mockRetrofit.create(FeedApi::class.java)
+        FakeFeedApi(delegate, context)
+    } else {
+        retrofit.create(FeedApi::class.java)
+    }
+
 }
